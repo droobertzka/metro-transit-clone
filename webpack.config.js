fad2008a69c33c72a2eb077189904771def9d196
droobertzka <droobertzka@gmail.com>
2021-01-28 00:42:49 -0600

    npm init, install and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	mode: 'development',
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'src/index.html'
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /.css$/,
+				use: [
+					{ loader: "style-loader" },
+					{ 
+						loader: "css-loader",
+						options: { sourceMap: true }
+					}
+				]
+			}
+		]
+	},
+	devServer: {
+		host: 'localhost',
+		hot: true,
+		proxy: {
+			'/nextripv2': {
+				target: 'https://svc.metrotransit.org',
+				secure: false,
+				changeOrigin: true
+			}
+		}
+	}
+}

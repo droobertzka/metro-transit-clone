46210554ea5e84b47093e410001656df99dbf656
droobertzka <droobertzka@gmail.com>
2021-01-30 17:19:27 -0600

    UI messages for missing stop data and no departures

--- src/components/departures.js
+++ src/components/departures.js
@@ -2,47 +2,51 @@ import { makeOnLoadError } from '../shared/events'
 import { $departures } from '../shared/elements'
 import { apiPath, fetchOpts, parseResponse } from '../shared/fetch-utils'
 
+const populateDeparture = ($list) => (departure) => {
+    const {
+        route_short_name,
+        terminal,
+        description,
+        departure_text
+    } = departure
+    const atOrIn = departure_text.toLowerCase().includes('min')
+        ? 'in'
+        : 'at'
+    const $item = document.createElement('li')
+    $item.innerHTML = `<strong>${
+            route_short_name}${terminal || ''
+        }</strong> departs for <strong>${
+            description
+        }</strong> ${atOrIn} <strong>${departure_text}</strong>`
+    $list.appendChild($item)
+}
+
 const onLoadDepartures = ({ stops, departures }) => {
     $departures.replaceChildren() // Admittedly not cross-compatible
     const stop = stops && stops[0]
+    const $header = document.createElement('h2')
     if (!stop) {
-        // TODO: No stop retrieved
+        $header.innerText = 'No data available for this stop'
         return
     }
 
     // Add header to departure section
     const { description, stop_id } = stop
-    const $header = document.createElement('h2')
     $header.innerHTML = `${description} <span class="stop-id">Stop #: ${
         stop_id
     }</span>`
     $departures.appendChild($header)
 
-    if (!departures || departures.length < 1) {
-        // TODO: No departures
-        return
+    const $list = document.createElement('ul')
+    if (departures && departures.length > 0) {
+        departures.forEach(populateDeparture($list))
+    } else {
+        const $item = document.createElement('li')
+        $item.innerText = 'No departures at this time'
+        $list.appendChild($item)
+        $departures.appendChild($list)
     }
 
-    // Populate departures
-    const $list = document.createElement('ul')
-    departures.forEach((departure) => {
-        const {
-            route_short_name,
-            terminal,
-            description,
-            departure_text
-        } = departure
-        const atOrIn = departure_text.toLowerCase().includes('min')
-            ? 'in'
-            : 'at'
-        const $item = document.createElement('li')
-        $item.innerHTML = `<strong>${
-                route_short_name}${terminal || ''
-            }</strong> departs for <strong>${
-                description
-            }</strong> ${atOrIn} <strong>${departure_text}</strong>`
-        $list.appendChild($item)
-    })
     $departures.appendChild($list)
 }
 
